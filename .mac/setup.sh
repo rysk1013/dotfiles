9ba5c87abeffb320bc36b966645a89509c1b3b95
dell-laptop-20210819-rysk <swpa0x33d@gmail.com>
2024-12-01 20:50:46 +0900

    update setup.sh

--- .mac/setup.sh
+++ .mac/setup.sh
@@ -1,45 +1,42 @@
 # homebrewのインストール
 /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
-echo 'export PATH="$PATH:/opt/homebrew/bin"' >> ~/.zsh.d/path.zsh
-source ~/.zshrc
-
-# キーボードの反応速度の修正(PC再起動後に有効化される)
-defaults write -g InitialKeyRepeat -int 11
-defaults write -g KeyRepeat -int 1
+echo >> /Users/$USER/.zprofile
+echo 'eval "$(/usr/local/bin/brew shellenv)"' >> /Users/$USER/.zprofile
+eval "$(/usr/local/bin/brew shellenv)"
 
 # アプリインストール
 # 入力
-brew install --cask google-japanese-ime
+brew install --cask "google-japanese-ime"
 # タッチパッドカスタマイズ
-brew install --cask bettertouchtool
+brew install --cask "bettertouchtool"
 # キーボードカスタマイズ
-brew install --cask karabiner-elements
+brew install --cask "karabiner-elements"
 # アプリアンインストール
-brew install --cask appcleaner
+brew install --cask "appcleaner"
 # ランチャー
-brew install --cask raycast
+brew install --cask "raycast"
 # ブラウザ
-brew install --cask google-chrome
-brew install --cask brave-browser
+brew install --cask "google-chrome"
+brew install --cask "brave-browser"
 # エディタ
-brew install --cask visual-studio-code
-brew install --cask visual-studio-code@insiders
+brew install --cask "visual-studio-code"
+brew install --cask "visual-studio-code@insiders"
 # メモ
-brew install --cask notion
+brew install --cask "notion"
 # ビデオミーティング
-brew install --cask zoom
+brew install --cask "zoom"
 # vpn
-brew install --cask protonvpn
+brew install --cask "protonvpn"
 # パスワードマネージャ
-brew install --cask bitwarden
+brew install --cask "bitwarden"
 
 # Apple StoreのアプリをCLIで管理
-brew install mas
+brew install "mas"
 # Xcode
-mas install 497799835
+mas install "497799835"
 # LINE
-mas install 539883307
+mas install "539883307"
 # Spark
-mas install 1176895641
+mas install "1176895641"
 # Activity Bar
-mas install 6447080018
+mas install "6447080018"
