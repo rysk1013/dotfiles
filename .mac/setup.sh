0177a6177487160eb9b43d0a00c326e0a17ea6ef
dell-laptop-20210819-rysk <swpa0x33d@gmail.com>
2024-12-01 12:20:35 +0900

    create .mac

---
+++ .mac/setup.sh
@@ -0,0 +1,45 @@
+# homebrewのインストール
+/bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
+echo 'export PATH="$PATH:/opt/homebrew/bin"' >> ~/.zsh.d/path.zsh
+source ~/.zshrc
+
+# キーボードの反応速度の修正(PC再起動後に有効化される)
+defaults write -g InitialKeyRepeat -int 11
+defaults write -g KeyRepeat -int 1
+
+# アプリインストール
+# ブラウザ
+brew install --cask google-chrome
+brew install --cask brave-browser
+# 入力
+brew install --cask google-japanese-ime
+# エディタ
+brew install --cask visual-studio-code
+brew install --cask visual-studio-code@insiders
+# メモ
+brew install --cask notion
+# ビデオミーティング
+brew install --cask zoom
+# タッチパッドカスタマイズ
+brew install --cask bettertouchtool
+# キーボードカスタマイズ
+brew install --cask karabiner-elements
+# アプリアンインストール
+brew install --cask appcleaner
+# ランチャー
+brew install --cask raycast
+# vpn
+brew install --cask protonvpn
+# パスワード管理
+brew install --cask bitwarden
+
+# Apple StoreのアプリをCLIで管理
+brew install mas
+# Xcode
+mas install 497799835
+# LINE
+mas install 539883307
+# Spark
+mas install 1176895641
+# Activity Bar
+mas install 6447080018
