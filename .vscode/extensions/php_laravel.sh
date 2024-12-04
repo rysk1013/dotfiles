da92096e14b4a3c8bb53b0d281d531f623fa5888
macbookpro-rysk <swpa0x33d@gmail.com>
2024-12-04 17:54:08 +0900

    create base.sh html_css_js.sh php_laravel.sh

---
+++ .vscode/extensions/php_laravel.sh
@@ -0,0 +1,19 @@
+#!/bin/sh
+
+# インストールする拡張機能
+extensions=(
+  'xdebug.php-debug'
+  'neilbrayfield.php-docblocker'
+  'bmewburn.vscode-intelephense-client'
+  'ryannaddy.laravel-artisan'
+  'onecentlin.laravel-blade'
+  'amiralizadeh9480.laravel-extra-intellisense'
+  'junstyle.php-cs-fixer'
+  'mehedidracula.php-namespace-resolver'
+  'brapifra.phpserver'
+  'persoderlind.vscode-phpcbf'
+)
+
+for extension in "${extensions[@]}"; do
+  code --install-extension $extension --force
+done
