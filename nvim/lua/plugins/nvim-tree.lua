0b908c207e8b73540b9f91eabb3012a4559e22ff
rysk(mac) <swpa0x33d@gmail.com>
2025-11-02 01:14:21 +0900

    update nvim/lua/plugins/nvim-tree.lua

--- nvim/lua/plugins/nvim-tree.lua
+++ nvim/lua/plugins/nvim-tree.lua
@@ -1,71 +1,100 @@
 return {
-	{
-		"nvim-tree/nvim-tree.lua",
-		dependencies = { "nvim-tree/nvim-web-devicons" },
-		version = "*",
-		lazy = false,
-		keys = {
-			{ "<leader>e", "<cmd>NvimTreeToggle<cr>", desc = "TreeToggle" },
-		},
-		config = function()
-			vim.g.loaded_netrw = 1
-			vim.g.loaded_netrwPlugin = 1
-
-			-- nvim-tree バッファ専用のキーマップを定義
-			local function my_on_attach(bufnr)
-				local api = require("nvim-tree.api")
-				local function opts(desc)
-					return { desc = "nvim-tree: " .. desc, buffer = bufnr, noremap = true, silent = true, nowait = true }
-				end
-
-				-- Enter で新しいタブに開く
-				vim.keymap.set("n", "<CR>", api.node.open.tab, opts("Open in Tab"))
-
-				-- 分割で開きたい場合は以下を有効化
-				-- vim.keymap.set("n", "v", api.node.open.vertical,   opts("Open Vertical Split"))
-				-- vim.keymap.set("n", "s", api.node.open.horizontal, opts("Open Horizontal Split"))
-			end
-
-			require("nvim-tree").setup({
-				on_attach = my_on_attach,
-				sort_by = "case_sensitive",
-				view = {
-					width = 30,
-					side = "left",
-					preserve_window_proportions = true,
-				},
-				renderer = {
-					group_empty = true,
-					highlight_git = true,
-					icons = {
-						show = {
-							file = true,
-							folder = true,
-							folder_arrow = true,
-							git = true,
+	"nvim-tree/nvim-tree.lua",
+	version = "*",
+	lazy = false,
+	dependencies = { "nvim-tree/nvim-web-devicons" }, -- アイコン表示用
+	config = function()
+		----------------------------------------------------------------
+		-- 基本設定
+		----------------------------------------------------------------
+		require("nvim-tree").setup({
+			sort_by = "case_sensitive",
+			view = {
+				width = 35,
+				side = "left",
+				signcolumn = "yes",
+				number = false,
+				relativenumber = false,
+				preserve_window_proportions = true,
+			},
+			renderer = {
+				root_folder_label = false,
+				highlight_opened_files = "none",
+				highlight_git = true,
+				indent_markers = { enable = true },
+				icons = {
+					show = {
+						file = true,
+						folder = true,
+						folder_arrow = true,
+						git = true,
+					},
+					glyphs = {
+						default = "󰈚",
+						symlink = "",
+						folder = {
+							default = "",
+							open = "",
+							empty = "",
+							empty_open = "",
+							arrow_closed = "",
+							arrow_open = "",
+						},
+						git = {
+							unstaged = "✗",
+							staged = "✓",
+							unmerged = "",
+							renamed = "➜",
+							untracked = "★",
+							deleted = "",
+							ignored = "◌",
 						},
 					},
 				},
-				filters = {
-					dotfiles = false,
-					git_ignored = false,
+			},
+			update_focused_file = {
+				enable = true,
+				update_root = false,
+			},
+			filters = {
+				dotfiles = false,
+				git_ignored = false,
+				custom = { ".git", "node_modules", ".cache" },
+			},
+			git = {
+				enable = true,
+				ignore = false,
+				timeout = 500,
+			},
+			actions = {
+				open_file = {
+					quit_on_open = false,
+					resize_window = true,
 				},
-				git = {
-					enable = true,
+				remove_file = { close_window = false },
+			},
+			diagnostics = {
+				enable = true,
+				show_on_dirs = true,
+				icons = {
+					hint = "",
+					info = "",
+					warning = "",
+					error = "",
 				},
-				actions = {
-					open_file = {
-						quit_on_open = false,
-						-- window_picker を無効にしたい場合は有効化:
-						-- window_picker = { enable = false },
-					},
-				},
-				update_focused_file = {
-					enable = true,
-					update_root = true,
-					ignore_list = {},
-				},
-			})
-		end,
-	},
+			},
+		})
+
+		----------------------------------------------------------------
+		-- Keymaps
+		----------------------------------------------------------------
+		local map = function(lhs, rhs, desc)
+			vim.keymap.set("n", lhs, rhs, { silent = true, noremap = true, desc = desc })
+		end
+
+		map("<leader>e", "<cmd>NvimTreeToggle<CR>", "Toggle file explorer")
+		map("<leader>o", "<cmd>NvimTreeFocus<CR>", "Focus file explorer")
+		map("<leader>r", "<cmd>NvimTreeRefresh<CR>", "Refresh file explorer")
+		map("<leader>n", "<cmd>NvimTreeFindFile<CR>", "Locate current file in tree")
+	end,
 }
