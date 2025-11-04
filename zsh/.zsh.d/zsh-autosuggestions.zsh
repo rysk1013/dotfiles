849874e825ca59f44fb530fb674921564a49de3d
rysk(mac) <swpa0x33d@gmail.com>
2025-11-04 19:31:54 +0900

    create zsh/.zsh.d/zsh-autosuggestions.zsh

---
+++ zsh/.zsh.d/zsh-autosuggestions.zsh
@@ -0,0 +1,6 @@
+#### zsh-autosuggestions ####
+ZSH_AUTOSUGGEST_HIGHLIGHT_STYLE='fg=#bcbcbc'
+ZSH_AUTOSUGGEST_STRATEGY=(history completion)
+ZSH_AUTOSUGGEST_BUFFER_MAX_SIZE=20
+ZSH_AUTOSUGGEST_ACCEPT_RIGHT_ARROW=1
+bindkey '→' autosuggest-accept
