8cba3431c5c1da7efe3736e5b06e92ee90cf2693
rysk(mac) <swpa0x33d@gmail.com>
2025-11-04 19:32:15 +0900

    create zsh/.zsh.d/zsh-syntax-highlighting.zsh

---
+++ zsh/.zsh.d/zsh-syntax-highlighting.zsh
@@ -0,0 +1,10 @@
+#### zsh-syntax-highlighting ####
+ZSH_HIGHLIGHT_HIGHLIGHTERS=(main)
+ZSH_HIGHLIGHT_STYLES[command]='fg=#a8ff60,bold'
+ZSH_HIGHLIGHT_STYLES[unknown-token]='fg=#ff5f87,bold'
+ZSH_HIGHLIGHT_STYLES[path]='fg=#5fd7ff'
+ZSH_HIGHLIGHT_STYLES[alias]='fg=#87d7ff,italic'
+ZSH_HIGHLIGHT_STYLES[reserved-word]='fg=#ffd75f,bold'
+ZSH_HIGHLIGHT_STYLES[globbing]='fg=#ffaf00'
+ZSH_HIGHLIGHT_STYLES[single-hyphen-option]='fg=#ffffaf'
+ZSH_HIGHLIGHT_STYLES[double-hyphen-option]='fg=#ffffaf'
