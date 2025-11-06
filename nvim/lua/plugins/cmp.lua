bd0289c2499e1581b9068563a2d5b957c5cdbc25
rysk(mac) <swpa0x33d@gmail.com>
2025-11-06 19:04:06 +0900

    update nvim/lua/plugins/cmp.lua

--- nvim/lua/plugins/cmp.lua
+++ nvim/lua/plugins/cmp.lua
@@ -26,7 +26,7 @@ return {
 				mapping = cmp.mapping.preset.insert({
 					["<C-n>"] = cmp.mapping.select_next_item(),
 					["<C-p>"] = cmp.mapping.select_prev_item(),
-					["<C-y>"] = cmp.mapping.confirm({ select = true }),
+					["<CR>"] = cmp.mapping.confirm({ select = true }),
 					["<C-Space>"] = cmp.mapping.complete(),
 					["q"] = cmp.mapping.abort(),
 				}),
