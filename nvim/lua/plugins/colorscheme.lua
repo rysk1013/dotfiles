0d2e51acac18c6a496a23e714a148fa782bb983f
rysk(mac) <swpa0x33d@gmail.com>
2025-11-11 22:40:40 +0900

    update nvim/lua/plugins/colorscheme.lua

--- nvim/lua/plugins/colorscheme.lua
+++ nvim/lua/plugins/colorscheme.lua
@@ -1,27 +1,34 @@
 return {
-  {
-    "lunarvim/darkplus.nvim",
-    lazy = false,
-    priority = 1000,
-    config = function()
-      -- テーマをロード
-      vim.cmd([[colorscheme darkplus]])
-      -- コメントの色を変更
-      local comment_color = "#999999"
+	{
+		"lunarvim/darkplus.nvim",
+		lazy = false,
+		priority = 1000,
+		config = function()
+			-- テーマをロード
+			vim.cmd([[colorscheme darkplus]])
 
-      -- treesitter対応（neovim 0.9以降）
-      vim.api.nvim_set_hl(0, "@comment", { fg = comment_color, italic = true })
-      -- 従来のcommentグループも上書き（互換用）
-      vim.api.nvim_set_hl(0, "comment",   { fg = comment_color, italic = true })
+			-- コメントの色を変更
+			local comment_color = "#999999"
+			-- treesitter対応（neovim 0.9以降）
+			vim.api.nvim_set_hl(0, "@comment", { fg = comment_color, italic = true })
+			-- 従来のcommentグループも上書き（互換用）
+			vim.api.nvim_set_hl(0, "comment", { fg = comment_color, italic = true })
+			-- テーマ変更時にも再適用されるように設定
+			vim.api.nvim_create_autocmd("colorscheme", {
+				callback = function()
+					vim.api.nvim_set_hl(0, "@comment", { fg = comment_color, italic = true })
+					vim.api.nvim_set_hl(0, "comment", { fg = comment_color, italic = true })
+				end,
+			})
 
-      -- テーマ変更時にも再適用されるように設定
-      vim.api.nvim_create_autocmd("colorscheme", {
-        callback = function()
-          vim.api.nvim_set_hl(0, "@comment", { fg = comment_color, italic = true })
-          vim.api.nvim_set_hl(0, "comment",   { fg = comment_color, italic = true })
-        end,
-      })
-    end,
-  },
+			-- 背景を全画面にする
+			vim.schedule(function()
+				vim.cmd([[
+          highlight! link NvimTreeNormal    Normal
+          highlight! link NvimTreeNormalNC  Normal
+          highlight! link NvimTreeEndOfBuffer EndOfBuffer
+        ]])
+			end)
+		end,
+	},
 }
-
