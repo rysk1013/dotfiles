b1ed2aecd217e37a2cd2b0c05acf57fcdbfb7f8d
rysk(mac) <swpa0x33d@gmail.com>
2025-11-11 20:17:09 +0900

    update nvim/lua/plugins/copilot.lua

--- nvim/lua/plugins/copilot.lua
+++ nvim/lua/plugins/copilot.lua
@@ -1,45 +1,48 @@
 return {
-  "zbirenbaum/copilot.lua",
-  cmd = "Copilot",
-  event = "InsertEnter",
-  -- ここで定義したキーを押した時点でも自動ロードされます
-  keys = {
-    { "<leader>ce", "<cmd>Copilot enable<CR>",  desc = "Copilot: Enable"  },
-    { "<leader>cd", "<cmd>Copilot disable<CR>", desc = "Copilot: Disable" },
-    { "<leader>cs", "<cmd>Copilot status<CR>",  desc = "Copilot: Status"  },
-    { "<leader>cp", "<cmd>Copilot panel<CR>",   desc = "Copilot: Panel"   },
-  },
-  config = function()
-    require("copilot").setup({
-      panel = {
-        enabled = true,
-        auto_refresh = true,
-        keymap = {
-          jump_prev = "[[",
-          jump_next = "]]",
-          accept    = "<CR>",
-          refresh   = "gr",
-          open      = "<M-p>",
-        },
-      },
-      suggestion = {
-        enabled = true,
-        auto_trigger = true,
-        debounce = 75,
-        keymap = {
-          accept      = "<Tab>",
-          accept_word = "<M-w>",
-          accept_line = "<M-l>",
-          next        = "<M-]>",
-          prev        = "<M-[>",
-          dismiss     = "<C-]>",
-        },
-      },
-      filetypes = {
-        gitcommit = true, markdown = true, yaml = true,
-        ["*"] = true, ["."] = false,
-      },
-      copilot_node_command = "node", -- 必要ならフルパスに
-    })
-  end,
+	"zbirenbaum/copilot.lua",
+	cmd = "Copilot",
+	event = "InsertEnter",
+	-- ここで定義したキーを押した時点でも自動ロードされます
+	keys = {
+		{ "<leader>ce", "<cmd>Copilot enable<CR>", desc = "Copilot: Enable" },
+		{ "<leader>cd", "<cmd>Copilot disable<CR>", desc = "Copilot: Disable" },
+		{ "<leader>cs", "<cmd>Copilot status<CR>", desc = "Copilot: Status" },
+		{ "<leader>cp", "<cmd>Copilot panel<CR>", desc = "Copilot: Panel" },
+	},
+	config = function()
+		require("copilot").setup({
+			panel = {
+				enabled = true,
+				auto_refresh = true,
+				keymap = {
+					jump_prev = "[[",
+					jump_next = "]]",
+					accept = "<CR>",
+					refresh = "gr",
+					open = "<M-p>",
+				},
+			},
+			suggestion = {
+				enabled = true,
+				auto_trigger = true,
+				debounce = 75,
+				keymap = {
+					accept = "<C-CR>",
+					accept_word = "<M-w>",
+					accept_line = "<M-l>",
+					next = "<M-]>",
+					prev = "<M-[>",
+					dismiss = "<C-]>",
+				},
+			},
+			filetypes = {
+				gitcommit = true,
+				markdown = true,
+				yaml = true,
+				["*"] = true,
+				["."] = false,
+			},
+			copilot_node_command = "node", -- 必要ならフルパスに
+		})
+	end,
 }
