382872a2d9eb9ca38c66ce7f63f850c5956abbb4
rysk(mac) <swpa0x33d@gmail.com>
2025-11-11 20:17:22 +0900

    update nvim/lua/plugins/nvim-tree.lua

--- nvim/lua/plugins/nvim-tree.lua
+++ nvim/lua/plugins/nvim-tree.lua
@@ -10,7 +10,7 @@ return {
 		require("nvim-tree").setup({
 			sort_by = "case_sensitive",
 			view = {
-				width = 30,
+				width = 25,
 				side = "left",
 				signcolumn = "yes",
 				number = false,
