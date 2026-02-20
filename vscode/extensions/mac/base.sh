ac4a922d7daede11f411c55d77b20a2751ffaa3c
rysk(mac) <swpa0x33d@gmail.com>
2026-02-20 22:39:22 +0900

    update vscode/extensions/mac/base.sh

--- vscode/extensions/mac/base.sh
+++ vscode/extensions/mac/base.sh
@@ -2,42 +2,46 @@
 
 # インストールする拡張機能
 extensions=(
-  'naumovs.color-highlight'
-  'ms-vscode-remote.remote-containers'
-  'ms-azuretools.vscode-docker'
-  'mikestead.dotenv'
-  'eamodio.gitlens'
-  'sgryjp.japanese-word-handler'
-  'ms-vsliveshare.vsliveshare'
-  'ms-vsliveshare.vsliveshare-pack'
-  'davidanson.vscode-markdownlint'
-  'pkief.material-icon-theme'
-  'christian-kohler.path-intellisense'
-  'esbenp.prettier-vscode'
-  'redhat.vscode-yaml'
-  'steoates.autoimport'
-  'wmaurer.change-case'
-  'sirtobi.code-clip-ring'
-  'mrmlnc.vscode-duplicate'
-  'editorconfig.editorconfig'
-  'dbaeumer.vscode-eslint'
-  'mhutchie.git-graph'
-  'donjayamanne.githistory'
-  'trixnz.go-to-method'
-  'oderwat.indent-rainbow'
-  'nickdemayo.vscode-json-editor'
-  'clemenspeters.format-json'
-  'nextfaze.json-parse-stringify'
-  'eriklynd.json-tools'
-  'ritwickdey.liveserver'
-  'ryu1kn.partial-diff'
-  'quicktype.quicktype'
-  'ionutvmi.path-autocomplete'
-  'johnpapa.vscode-peacock'
-  'arjun.swagger-viewer'
-  'shardulm94.trailing-spaces'
-  'mosapride.zenkaku'
-  'wayou.vscode-todo-highlight'
+  # Git・バージョン管理
+  'eamodio.gitlens',
+  'mhutchie.git-graph',
+  'donjayamanne.githistory',
+  'ryu1kn.partial-diff',
+  # コンテナ・リモート開発
+  'ms-vscode-remote.remote-containers',
+  'ms-azuretools.vscode-docker',
+  # コード品質・Lint・フォーマット
+  'esbenp.prettier-vscode',
+  'dbaeumer.vscode-eslint',
+  'davidanson.vscode-markdownlint',
+  'editorconfig.editorconfig',
+  'clemenspeters.format-json',
+  'eriklynd.json-tools',
+  # パス補完・インポート支援
+  'christian-kohler.path-intellisense',
+  'ionutvmi.path-autocomplete',
+  'steoates.autoimport',
+  'trixnz.go-to-method',
+  # UI・表示カスタマイズ
+  'pkief.material-icon-theme',
+  'johnpapa.vscode-peacock',
+  'oderwat.indent-rainbow',
+  'naumovs.color-highlight',
+  'wayou.vscode-todo-highlight',
+  # JSON・API・データ系
+  'nickdemayo.vscode-json-editor',
+  'nextfaze.json-parse-stringify',
+  'quicktype.quicktype',
+  'arjun.swagger-viewer',
+  'redhat.vscode-yaml',
+  # ユーティリティ
+  'shardulm94.trailing-spaces',
+  'mikestead.dotenv',
+  'wmaurer.change-case',
+  'sirtobi.code-clip-ring',
+  'mrmlnc.vscode-duplicate',
+  'sgryjp.japanese-word-handler',
+  'mosapride.zenkaku',
 )
 
 for extension in "${extensions[@]}"; do
