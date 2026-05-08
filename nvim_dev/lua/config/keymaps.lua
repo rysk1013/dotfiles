529c3ca5e2cc44b7ca3b9115be097b4461f8b21d
rysk(mac) <swpa0x33d@gmail.com>
2026-05-08 22:23:09 +0900

    create nvim_dev

---
+++ nvim_dev/lua/config/keymaps.lua
@@ -0,0 +1,65 @@
+-- keymap.set を短く書けるようにする
+local keymap = vim.keymap.set
+-- 共通オプション
+local opts = { noremap = true, silent = true }
+-- =========================
+-- 基本操作
+-- =========================
+-- jj で INSERT モードを抜ける
+keymap("i", "jj", "<Esc>", opts)
+-- <Esc> で検索ハイライトを消す
+keymap("n", "<Esc>", "<cmd>nohlsearch<CR>", opts)
+-- =========================
+-- ウィンドウ移動
+-- =========================
+-- Ctrl+h で左のウィンドウへ移動
+keymap("n", "<C-h>", "<C-w>h", opts)
+-- Ctrl+j で下のウィンドウへ移動
+keymap("n", "<C-j>", "<C-w>j", opts)
+-- Ctrl+k で上のウィンドウへ移動
+keymap("n", "<C-k>", "<C-w>k", opts)
+-- Ctrl+l で右のウィンドウへ移動
+keymap("n", "<C-l>", "<C-w>l", opts)
+-- =========================
+-- ウィンドウ分割
+-- =========================
+-- <leader>sv で縦分割
+keymap("n", "<leader>sv", "<cmd>vsplit<CR>", opts)
+-- <leader>sh で横分割
+keymap("n", "<leader>sh", "<cmd>split<CR>", opts)
+-- <leader>sx で現在の分割を閉じる
+keymap("n", "<leader>sx", "<cmd>close<CR>", opts)
+-- =========================
+-- バッファ操作
+-- =========================
+-- <leader>bd で現在のバッファを閉じる
+keymap("n", "<leader>bd", "<cmd>bdelete<CR>", opts)
+-- <Tab> で次のバッファへ
+keymap("n", "<Tab>", "<cmd>bnext<CR>", opts)
+-- <S-Tab> で前のバッファへ
+keymap("n", "<S-Tab>", "<cmd>bprevious<CR>", opts)
+-- =========================
+-- ファイル操作
+-- =========================
+-- <leader>w で保存
+keymap("n", "<leader>w", "<cmd>write<CR>", opts)
+-- <leader>q で終了
+keymap("n", "<leader>q", "<cmd>quit<CR>", opts)
+-- =========================
+-- 行移動
+-- =========================
+-- Visualモードで選択行を下へ移動
+keymap("v", "J", ":m '>+1<CR>gv=gv", opts)
+-- Visualモードで選択行を上へ移動
+keymap("v", "K", ":m '<-2<CR>gv=gv", opts)
+-- =========================
+-- 便利操作
+-- =========================
+-- x で削除してもレジスタに入れない
+keymap("n", "x", '"_x', opts)
+-- <leader>y でシステムクリップボードへコピー
+keymap({ "n", "v" }, "<leader>y", '"+y', opts)
+-- <leader>Y で行全体をシステムクリップボードへコピー
+keymap("n", "<leader>Y", '"+Y', opts)
+-- <leader>d で削除してもレジスタに入れない
+keymap({ "n", "v" }, "<leader>d", '"_d', opts)
---
+++ nvim_dev/lua/config/options.lua
@@ -0,0 +1,112 @@
+-- vim.opt を短く書けるように変数へ代入
+local opt = vim.opt
+
+-- =========================
+-- Leader Key
+-- =========================
+-- LeaderキーをSpaceへ設定
+vim.g.mapleader = " "
+-- LocalLeaderキーをSpaceへ設定
+vim.g.maplocalleader = "\\"
+-- =========================
+-- 行番号
+-- =========================
+-- 絶対行番号を表示
+opt.number = true
+-- カーソル行からの相対行番号を表示
+opt.relativenumber = true
+-- =========================
+-- インデント
+-- =========================
+-- Tab入力をスペースへ変換
+opt.expandtab = true
+-- Tab文字の表示幅
+opt.tabstop = 2
+-- << や >> のインデント幅
+opt.shiftwidth = 2
+-- Tabキー押下時のスペース数
+opt.softtabstop = 2
+-- 自動インデントを有効化
+opt.smartindent = true
+-- =========================
+-- 表示
+-- =========================
+-- 長い行を折り返さない
+opt.wrap = false
+-- カーソル行をハイライト
+opt.cursorline = true
+-- 常にサインカラムを表示
+opt.signcolumn = "yes"
+-- True Color を有効化
+opt.termguicolors = true
+-- 上下に最低8行の余白を確保
+opt.scrolloff = 8
+-- 左右スクロール時の余白
+opt.sidescrolloff = 8
+-- =========================
+-- 検索
+-- =========================
+-- 検索時に大文字小文字を区別しない
+opt.ignorecase = true
+-- 検索語に大文字が含まれる場合のみ区別する
+opt.smartcase = true
+-- 検索結果をハイライト
+opt.hlsearch = true
+-- 入力中にインクリメンタル検索
+opt.incsearch = true
+-- =========================
+-- 操作
+-- =========================
+-- マウス操作を有効化
+opt.mouse = "a"
+-- OSクリップボードと共有
+opt.clipboard = "unnamedplus"
+-- キーマップ待機時間（ms）
+opt.timeoutlen = 300
+-- 更新イベント発火までの時間（ms）
+opt.updatetime = 250
+-- =========================
+-- ウィンドウ分割
+-- =========================
+-- 垂直分割を右側に開く
+opt.splitright = true
+-- 水平分割を下側に開く
+opt.splitbelow = true
+-- =========================
+-- ファイル管理
+-- =========================
+-- swapファイルを作成しない
+opt.swapfile = false
+-- バックアップファイルを作成しない
+opt.backup = false
+-- 保存前バックアップを無効化
+opt.writebackup = false
+-- undo履歴をファイル保存後も保持
+opt.undofile = true
+-- =========================
+-- 補完
+-- =========================
+-- 補完メニューの表示設定
+opt.completeopt = {
+  "menu",      -- 補完メニューを表示
+  "menuone",   -- 候補が1件でも表示
+  "noselect",  -- 自動選択しない
+}
+-- =========================
+-- コマンドライン
+-- =========================
+-- コマンドラインの高さ
+opt.cmdheight = 1
+-- INSERTモード表示 (-- INSERT --) を非表示
+opt.showmode = false
+-- =========================
+-- 不可視文字
+-- =========================
+-- 不可視文字を表示
+opt.list = true
+-- 不可視文字の表示内容
+opt.listchars = {
+  tab = "» ",   -- Tab文字
+  trail = "·",  -- 行末スペース
+  nbsp = "␣",   -- ノーブレークスペース
+}
