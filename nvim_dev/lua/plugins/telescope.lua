2d10e8d516b16d7ce1f66f0994cb89c6858d8323
rysk(mac) <swpa0x33d@gmail.com>
2026-05-12 22:48:57 +0900

    create nvim_dev/lua/plugins/telescope.lua

---
+++ nvim_dev/lua/plugins/telescope.lua
@@ -0,0 +1,33 @@
+return {
+  {
+    "nvim-telescope/telescope.nvim", 
+
+     version = "*",
+
+    dependencies = {
+      "nvim-lua/plenary.nvim",
+      { "nvim-telescope/telescope-fzf-native.nvim", build = "make" },
+    },
+
+    keys = {
+      { "<leader>ff", "<CMD>Telescope find_files<CR>", mode = "n", desc = "Telescope find files" },
+      { "<leader>fg", "<CMD>Telescope live_grep<CR>", mode = "n", desc = "Telescope live grep" },
+      { "<leader>fb", "<CMD>Telescope buffers<CR>", mode = "n", desc = "Telescope buffers" },
+      { "<leader>fh", "<CMD>Telescope help_tags<CR>", mode = "n", desc = "Telescope help tags" },
+    },
+
+    config = function()
+      require("telescope").setup({
+        defaults = {
+          layout_strategy = "horizontal",
+          layout_config = {
+            horizontal = {
+              prompt_position = "top",
+            },
+          },
+          sorting_strategy = "ascending",
+        },
+      })
+    end
+  }
+}
