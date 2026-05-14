9c8cfa907378a38c1c44fb7ec1e9813a6831eed1
rysk(mac) <swpa0x33d@gmail.com>
2026-05-14 20:51:40 +0900

    update nvim_dev/lua/plugins/oil.lua

--- nvim_dev/lua/plugins/oil.lua
+++ nvim_dev/lua/plugins/oil.lua
@@ -1,5 +1,16 @@
 return {
   {
+    "FerretDetective/oil-git-signs.nvim",
+
+    ft = "oil",
+
+    opts = {
+      show_ignored = function()
+        return false
+      end
+    },
+  },
+  {
     "stevearc/oil.nvim",
 
     lazy = false,
@@ -15,7 +26,11 @@ return {
       skip_confirm_for_simple_edits = true,
       view_options = {
         show_hidden = true,
-      }
+      },
+      win_options = {
+        signcolumn = "yes:2",
+        statuscolumn = "",
+      },
     },
   }
 }
