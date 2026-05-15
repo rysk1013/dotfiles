1b68e03e0b7f2c958963b1decebb542016be4751
rysk(mac) <swpa0x33d@gmail.com>
2026-05-15 23:16:13 +0900

    update nvim_dev/lua/plugins/colorscheme.lua

--- nvim_dev/lua/plugins/colorscheme.lua
+++ nvim_dev/lua/plugins/colorscheme.lua
@@ -1,25 +1,19 @@
 return {
   {
-    "bluz71/vim-moonfly-colors",
+    "Mofiqul/vscode.nvim",
 
     lazy = false,
 
     priority = 1000,
 
-    config = function()
-      vim.g.moonflyTransparent = true
-      vim.cmd.colorscheme("moonfly")
+    opts = {
+      transparent = true,
+    },
+
+    config = function(_, opts)
+     require("vscode").setup(opts)
+
+     vim.cmd.colorscheme("vscode")
     end
-
-    -- Other settings
-    --opts = {
-    --  transparent = true,
-    --},
-
-    -- config = function(_, opts)
-    --   require("vscode").setup(opts)
-
-    --   vim.cmd.colorscheme("vscode")
-    -- end
   }
 }
