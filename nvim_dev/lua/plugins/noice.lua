a83886458e00713c924414b85f7db0347a2d57b7
rysk(mac) <swpa0x33d@gmail.com>
2026-05-15 23:49:12 +0900

    update nvim_dev/lua/plugins/noice.lua

--- nvim_dev/lua/plugins/noice.lua
+++ nvim_dev/lua/plugins/noice.lua
@@ -4,7 +4,23 @@ return {
 
     event = "VeryLazy",
 
-    opts = {},
+    opts = {
+      views = {
+        cmdline_popup = {
+          position = {
+            row = "20%",
+            col = "50%",
+          },
+        },
+      },
+      lsp = {
+        override = {
+          ["vim.lsp.util.convert_input_to_markdown_lines"] = true,
+          ["vim.lsp.util.stylize_markdown"] = true,
+          ["cmp.entry.get_documentation"] = true,
+        },
+      },
+    },
 
     dependencies = {
       "MunifTanjim/nui.nvim",
