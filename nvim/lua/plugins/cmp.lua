c0047edca8db8eeda1daf0d71d9f0079cdbe6a79
rysk(mac) <swpa0x33d@gmail.com>
2026-05-19 21:56:04 +0900

    create nvim/lua/plugins/cmp.lua

---
+++ nvim/lua/plugins/cmp.lua
@@ -0,0 +1,76 @@
+return {
+  {
+    "hrsh7th/nvim-cmp",
+
+    dependencies = {
+     "hrsh7th/cmp-nvim-lsp",
+     "hrsh7th/cmp-buffer",
+     "hrsh7th/cmp-path",
+     "hrsh7th/cmp-cmdline",
+    },
+
+    config = function()
+      local cmp = require("cmp")
+
+      cmp.setup({
+        snippet = {
+          expand = function(args)
+            vim.fn["vsnip#anonymous"](args.body)
+          end,
+        },
+
+        window = {
+          completion = cmp.config.window.bordered(),
+          documentation = cmp.config.window.bordered(),
+        },
+
+        mapping = cmp.mapping.preset.insert({
+          ["<C-b>"] = cmp.mapping.scroll_docs(-4),
+          ["<C-f>"] = cmp.mapping.scroll_docs(4),
+          ["<C-Space>"] = cmp.mapping.complete(),
+          ["<C-e>"] = cmp.mapping.abort(),
+          ["<CR>"] = cmp.mapping.confirm({ select = true }),
+        }),
+
+        formatting = {
+          fields = { "abbr", "menu" },
+          format = function(entry, vim_item)
+            vim_item.menu = ({
+              nvim_lsp = "[LSP]",
+              buffer = "[Buffer]",
+              path = "[Path]",
+              luasnip = "[Snippet]",
+            })[entry.source.name]
+            return vim_item
+          end,
+        },
+
+        sources = cmp.config.sources({
+          { name = "nvim_lsp" },
+          { name = "vsnip" },
+        }, {
+          { name = "buffer" },
+          { name = "path" },
+          { name = "luasnip" },
+        })
+      })
+
+      cmp.setup.cmdline({ "/", "?" }, {
+        mapping = cmp.mapping.preset.cmdline(),
+        sources = {
+          { name = "buffer" }
+        }
+      })
+
+      cmp.setup.cmdline(":", {
+        mapping = cmp.mapping.preset.cmdline(),
+        sources = cmp.config.sources({
+          { name = "path" }
+        }, {
+          { name = "cmdline" }
+        }),
+        matching = { disallow_symbol_nonprefix_matching = false }
+      })
+    end
+  }
+}
