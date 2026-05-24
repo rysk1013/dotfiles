f0c1911af29530dca3dab5ebd92352c7861ffdab
rysk(mac) <swpa0x33d@gmail.com>
2026-05-24 16:12:04 +0900

    update nvim/lua/plugins/colorscheme.lua

--- nvim/lua/plugins/colorscheme.lua
+++ nvim/lua/plugins/colorscheme.lua
@@ -1,22 +1,5 @@
 return {
   {
-    "EdenEast/nightfox.nvim",
-
-    lazy = false,
-
-    priority = 1000,
-
-    config = function()
-     require("nightfox").setup({
-        options = {
-          transparent = true,
-        },
-     })
-
-     vim.cmd.colorscheme("carbonfox")
-
-     -- customize visual mode highlight
-     vim.api.nvim_set_hl(0, 'Visual', { bg = '#5e6480', fg = '#ffffff' })
-    end
+    require("plugins.colorscheme.nightfox"),
   }
 }
