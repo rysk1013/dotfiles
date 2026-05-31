96025f363f48e7464ec243352a8fdc0ff458ef2f
rysk(mac) <swpa0x33d@gmail.com>
2026-06-01 00:32:05 +0900

    update nvim/lua/plugins/lualine.lua

--- nvim/lua/plugins/lualine.lua
+++ nvim/lua/plugins/lualine.lua
@@ -5,11 +5,29 @@ return {
     dependencies = { "nvim-tree/nvim-web-devicons" },
 
     config = function()
+      local custom_section_separator = { left = "", right = "" }
+
       require("lualine").setup({
         options = {
           theme = "powerline_dark",
+          component_separators = { left = "", right = "" },
         },
         sections = {
+          lualine_a = {
+            {
+              "mode",
+              separator = custom_section_separator,
+            },
+          },
+          lualine_b = {
+            {
+              "branch",
+              "diff",
+              "diagnostics",
+              separator = custom_section_separator,
+              color = { bg = "#5c6d74" },
+            },
+          },
           lualine_c = {
             {
               "filename",
@@ -28,6 +46,19 @@ return {
             },
             "filetype",
           },
+          lualine_y = {
+            {
+              "progress",
+              separator = custom_section_separator,
+              color = { bg = "#5c6d74" },
+            },
+          },
+          lualine_z = {
+            {
+              "location",
+              separator = custom_section_separator,
+            },
+          }
         }
       })
     end
