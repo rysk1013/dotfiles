7b59296892321aa726f2318e3a3e3d39349eac04
rysk(mac) <swpa0x33d@gmail.com>
2026-06-03 21:27:18 +0900

    update nvim/lua/plugins/lsp-config.lua

--- nvim/lua/plugins/lsp-config.lua
+++ nvim/lua/plugins/lsp-config.lua
@@ -1,30 +1,32 @@
 return {
-  "neovim/nvim-lspconfig",
+  {
+    "neovim/nvim-lspconfig",
 
-  event = { "BufReadPre", "BufNewFile" },
+    event = { "BufReadPre", "BufNewFile" },
 
-  dependencies = {
-    "mason-org/mason-lspconfig.nvim",
-  },
+    dependencies = {
+      "mason-org/mason-lspconfig.nvim",
+    },
 
-  config = function()
-    local servers = require("config.lsp_servers")
+    config = function()
+      local servers = require("config.lsp_servers")
 
-    for server, config in pairs(servers) do
-      vim.lsp.config(server, config)
-      vim.lsp.enable(server)
+      for server, config in pairs(servers) do
+        vim.lsp.config(server, config)
+        vim.lsp.enable(server)
+      end
+
+      vim.diagnostic.config({
+        virtual_text = true,
+        signs = true,
+        underline = true,
+        update_in_insert = false,
+        severity_sort = true,
+        float = {
+          border = "rounded",
+          source = "if_many",
+        }
+      })
     end
-
-    vim.diagnostic.config({
-      virtual_text = true,
-      signs = true,
-      underline = true,
-      update_in_insert = false,
-      severity_sort = true,
-      float = {
-        border = "rounded",
-        source = "if_many",
-      }
-    })
-  end
+  }
 }
