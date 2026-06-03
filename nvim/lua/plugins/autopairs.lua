ab8caf3d0641469881ed766b8f228c8e2e6ed668
rysk(mac) <swpa0x33d@gmail.com>
2026-06-03 21:26:19 +0900

    update nvim/lua/plugins/autopairs.lua

--- nvim/lua/plugins/autopairs.lua
+++ nvim/lua/plugins/autopairs.lua
@@ -1,9 +1,9 @@
 return {
   {
-      'windwp/nvim-autopairs',
+    'windwp/nvim-autopairs',
 
-      event = "InsertEnter",
+    event = "InsertEnter",
 
-      config = true
+    config = true
   }
 }
