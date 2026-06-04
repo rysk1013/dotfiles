81cb34ceed25bcc0bb325e2cbf8e37182f9812e5
rysk(mac) <swpa0x33d@gmail.com>
2026-06-04 20:27:50 +0900

    update nvim/lua/plugins/lualine.lua

--- nvim/lua/plugins/lualine.lua
+++ nvim/lua/plugins/lualine.lua
@@ -5,12 +5,25 @@ return {
     dependencies = { "nvim-tree/nvim-web-devicons" },
 
     config = function()
+      local function is_git_repo()
+        local name = vim.api.nvim_buf_get_name(0)
+        if name == "" then
+          return false
+        end
+
+        local dir = vim.fs.dirname(name)
+        return vim.fs.root(dir, ".git") ~= nil
+      end
+
       local section_separator = { left = "", right = "" }
       local white_separator = {
         function() return "" end,
         padding = 0,
         color = { fg = "#ffffff", bg = "NONE" },
       }
+      local git_white_saparetor = vim.tbl_extend("force", white_separator, {
+        cond = is_git_repo,
+      })
 
       require("lualine").setup({
         options = {
@@ -25,7 +38,7 @@ return {
             },
           },
           lualine_b = {
-            white_separator,
+            git_white_saparetor,
             {
               "branch",
               "diff",
