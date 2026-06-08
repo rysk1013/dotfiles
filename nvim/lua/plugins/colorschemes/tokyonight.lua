c928470c7c039ee482a0b9234160cb05f733ecf8
rysk(mac) <swpa0x33d@gmail.com>
2026-06-09 01:14:18 +0900

    create nvim/lua/plugins/colorschemes

---
+++ nvim/lua/plugins/colorschemes/tokyonight.lua
@@ -0,0 +1,25 @@
+return {
+  "folke/tokyonight.nvim",
+
+  lazy = false,
+
+  priority = 1000,
+
+  config = function()
+    require("tokyonight").setup({
+      transparent = true,
+
+      on_highlights = function(highlights, colors)
+        highlights.LineNr = { fg = "#808080" }
+        highlights.LineNrAbove = { fg = "#808080" }
+        highlights.LineNrBelow = { fg = "#808080" }
+        highlights.CursorLineNr = { fg = "#00bfff" }
+        highlights.Visual = { bg = "#5e6480", fg = "#ffffff" }
+        highlights.Comment = { fg = "#6d7aba", italic = true }
+        highlights.DiagnosticUnnecessary = { fg = "#708090" }
+      end,
+    })
+
+    vim.cmd.colorscheme("tokyonight")
+  end
+}
