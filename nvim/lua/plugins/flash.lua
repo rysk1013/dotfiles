bb3974adef5ed79f36856e19dad8c2817a006c39
rysk(mac) <swpa0x33d@gmail.com>
2026-06-27 21:31:20 +0900

    update nvim/lua/plugins/flash.lua

--- nvim/lua/plugins/flash.lua
+++ nvim/lua/plugins/flash.lua
@@ -4,8 +4,6 @@ return {
 
     event = "VeryLazy",
 
-    opts = {},
-
     keys = {
       { "s", mode = { "n", "x", "o" }, function() require("flash").jump() end, desc = "Flash" },
       { "S", mode = { "n", "x", "o" }, function() require("flash").treesitter() end, desc = "Flash Treesitter" },
@@ -13,5 +11,7 @@ return {
       { "R", mode = { "o", "x" }, function() require("flash").treesitter_search() end, desc = "Treesitter Search" },
       { "<c-s>", mode = { "c" }, function() require("flash").toggle() end, desc = "Toggle Flash Search" },
     },
+
+    opts = {},
   }
 }
