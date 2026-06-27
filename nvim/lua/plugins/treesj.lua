8945f3ee3a1d71011509ac09f520234e946c48b8
rysk(mac) <swpa0x33d@gmail.com>
2026-06-27 21:34:05 +0900

    update nvim/lua/plugins/treesj.lua

--- nvim/lua/plugins/treesj.lua
+++ nvim/lua/plugins/treesj.lua
@@ -2,13 +2,13 @@ return {
   {
     "Wansmer/treesj",
 
+    dependencies = { "nvim-treesitter/nvim-treesitter" },
+
     keys = {
       { "<leader>m", function() require("treesj").toggle() end, mode = "n", desc = "TreeSJ toggle" },
       { "<leader>j", function() require("treesj").split() end, mode = "n", desc = "TreeSJ split" },
       { "<leader>s", function() require("treesj").join() end, mode = "n", desc = "TreeSJ join" },
     },
-
-    dependencies = { "nvim-treesitter/nvim-treesitter" },
 
     config = function()
       require("treesj").setup({
