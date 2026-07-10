3c82a258a596bd314fd66f906a9b8f6ef20696a0
rysk(mac) <swpa0x33d@gmail.com>
2026-07-10 23:47:57 +0900

    update zsh/.zsh.d/eval.zsh

--- zsh/.zsh.d/eval.zsh
+++ zsh/.zsh.d/eval.zsh
@@ -4,6 +4,9 @@ eval "$(starship init zsh)"
 #### sheldon ####
 eval "$(sheldon source)"
 
+#### mise ####
+eval "$(mise activate zsh)"
+
 #### kiro ####
 [[ "$TERM_PROGRAM" == "kiro" ]] && . "$(kiro --locate-shell-integration-path zsh)"
 
