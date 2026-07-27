c0748d1893894af6182ced4cb58069b65a852e6d
rysk(mac) <swpa0x33d@gmail.com>
2026-07-27 21:26:00 +0900

    fix vscode/extensions/windows/

---
+++ vscode/extensions/windows/html_css_js.ps1
@@ -0,0 +1,25 @@
+# インストールする拡張機能
+$extensions = @(
+    'formulahendry.auto-close-tag'
+  'formulahendry.auto-rename-tag'
+  'vincaslt.highlight-matching-tag'
+  'pranaygp.vscode-css-peek'
+  'xabikos.javascriptsnippets'
+  'onecentlin.laravel5-snippets'
+)
+
+# カレントにパスが通っていない場合は通す
+if (-not $env:path.Split(';').Contains('.\')) {
+    $env:path = $env:path + ';.\'
+}
+
+# binへのパスが未登録ならカレントディレクトリ変更
+$binPath = Join-Path ([Environment]::GetFolderPath('LocalApplicationData')) 'Programs\Microsoft VS Code\bin'
+if ($env:path.IndexOf($binpath, [System.StringComparison]::OrdinalIgnoreCase) -eq -1) {
+    cd $binPath
+}
+
+# 拡張機能のインストール実行
+foreach ($extension in $extensions) {
+  code --install-extension $extension --force
+}
---
+++ vscode/extensions/windows/php_laravel.ps1
@@ -0,0 +1,29 @@
+# インストールする拡張機能
+$extensions = @(
+  'xdebug.php-debug'
+  'neilbrayfield.php-docblocker'
+  'bmewburn.vscode-intelephense-client'
+  'ryannaddy.laravel-artisan'
+  'onecentlin.laravel-blade'
+  'amiralizadeh9480.laravel-extra-intellisense'
+  'junstyle.php-cs-fixer'
+  'mehedidracula.php-namespace-resolver'
+  'brapifra.phpserver'
+  'persoderlind.vscode-phpcbf'
+)
+
+# カレントにパスが通っていない場合は通す
+if (-not $env:path.Split(';').Contains('.\')) {
+    $env:path = $env:path + ';.\'
+}
+
+# binへのパスが未登録ならカレントディレクトリ変更
+$binPath = Join-Path ([Environment]::GetFolderPath('LocalApplicationData')) 'Programs\Microsoft VS Code\bin'
+if ($env:path.IndexOf($binpath, [System.StringComparison]::OrdinalIgnoreCase) -eq -1) {
+    cd $binPath
+}
+
+# 拡張機能のインストール実行
+foreach ($extension in $extensions) {
+  code --install-extension $extension --force
+}
