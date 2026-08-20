c7d7171967a6206a24436a5240e302f0a46f9f7e
rysk(mac) <swpa0x33d@gmail.com>
2026-08-20 21:00:45 +0900

    update nvim/lua/plugins/telescope.lua

--- nvim/lua/plugins/telescope.lua
+++ nvim/lua/plugins/telescope.lua
@@ -1,8 +1,27 @@
+local function get_search_cwd()
+  -- Oilの場合
+  if vim.bo.filetype == "oil" then
+    local dir = require("oil").get_current_dir()
+    if dir then
+      return dir
+    end
+  end
+
+  -- 通常ファイルの場合
+  local path = vim.api.nvim_buf_get_name(0)
+  if path ~= "" then
+    return vim.fs.dirname(path)
+  end
+
+  -- 新規バッファの場合
+  return vim.fn.getcwd()
+end
+
 return {
   {
     "nvim-telescope/telescope.nvim",
 
-     version = "*",
+    version = "*",
 
     dependencies = {
       "nvim-lua/plenary.nvim",
@@ -10,8 +29,18 @@ return {
     },
 
     keys = {
-      { "<leader>ff", "<cmd>Telescope find_files<CR>", mode = "n", desc = "Telescope find files" },
-      { "<leader>fg", "<cmd>Telescope live_grep<CR>", mode = "n", desc = "Telescope live grep" },
+      {
+        "<leader>ff",
+        function() require("telescope.builtin").find_files({ cwd = get_search_cwd() }) end,
+        mode = "n",
+        desc = "Telescope find files (current dir)",
+      },
+      {
+        "<leader>fg",
+        function() require("telescope.builtin").live_grep({ cwd = get_search_cwd() }) end,
+        mode = "n",
+        desc = "Telescope live grep (current dir)",
+      },
       { "<leader>fb", "<cmd>Telescope buffers<CR>", mode = "n", desc = "Telescope buffers" },
       { "<leader>fh", "<cmd>Telescope help_tags<CR>", mode = "n", desc = "Telescope help tags" },
     },
@@ -28,6 +57,6 @@ return {
           sorting_strategy = "ascending",
         },
       })
-    end
+    end,
   }
 }
