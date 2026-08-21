2c0c286c168fe2ac9f60fad595db154584592af2
rysk(mac) <swpa0x33d@gmail.com>
2026-08-21 21:17:21 +0900

    update wezterm/keybinds.lua

--- wezterm/keybinds.lua
+++ wezterm/keybinds.lua
@@ -49,8 +49,6 @@ return {
 				end),
 			}),
 		},
-		-- コマンドパレット表示
-		{ key = "p", mods = "SUPER", action = act.ActivateCommandPalette },
 		-- Tab移動
 		{ key = "Tab", mods = "CTRL", action = act.ActivateTabRelative(1) },
 		{ key = "Tab", mods = "SHIFT|CTRL", action = act.ActivateTabRelative(-1) },
